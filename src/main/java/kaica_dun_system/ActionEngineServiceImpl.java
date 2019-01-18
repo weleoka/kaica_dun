45f3a84940c040a49ecf99d6445f50bc42968420
Carl Granstrom <carl.grnstrm@gmail.com>
2019-01-18 19:13:08 +0100

    Created new branch to implement the menu functionality for looting items.

--- src/main/java/kaica_dun_system/ActionEngineServiceImpl.java
+++ src/main/java/kaica_dun_system/ActionEngineServiceImpl.java
@@ -14,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.util.Set;
 
